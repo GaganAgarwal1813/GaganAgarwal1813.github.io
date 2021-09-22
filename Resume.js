8ca17b87c4e212d5d1318acc46d6c5878babcdfd
GaganAgarwal1813 <48082205+GaganAgarwal1813@users.noreply.github.com>
2021-09-22 18:19:11 +0530

    Update Resume.js

--- Resume.js
+++ Resume.js
@@ -1,3 +1,93 @@
+let homevar=document.getElementById("home");
+let skillvar=document.getElementById("skill");
+let experiencevar=document.getElementById("experience");
+let educationvar=document.getElementById("education");
+let portfoliovar=document.getElementById("portfolio");
+let contactvar=document.getElementById("Contact");
+skillvar.style.display="none";
+experiencevar.style.display="none";
+educationvar.style.display="none";
+portfoliovar.style.display="none";
+contactvar.style.display="none";
+
+
+function myfunctionhome() {
+	//var x = document.getElementById("myDIV");
+	if (homevar.style.display === "none") {
+		homevar.style.display = "block";
+		skillvar.style.display = "none";
+		experiencevar.style.display = "none";
+		educationvar.style.display = "none";
+		portfoliovar.style.display = "none";
+		contactvar.style.display = "none";
+	} 
+  } 
+
+  function myfunctionskill() {
+	//var x = document.getElementById("myDIV");
+	if (skillvar.style.display === "none") {
+		skillvar.style.display = "block";
+		homevar.style.display = "none";
+		experiencevar.style.display = "none";
+		educationvar.style.display = "none";
+		portfoliovar.style.display = "none";
+		contactvar.style.display = "none";
+	} 
+  } 
+
+function myfunctionexperience()
+{
+	if (experiencevar.style.display === "none") {
+		experiencevar.style.display = "block";
+		homevar.style.display = "none";
+		skillvar.style.display = "none";
+		educationvar.style.display = "none";
+		portfoliovar.style.display = "none";
+		contactvar.style.display = "none";
+	} 
+}
+
+function myfunctioneducation()
+{
+	if (educationvar.style.display === "none") {
+		educationvar.style.display = "block";
+		homevar.style.display = "none";
+		skillvar.style.display = "none";
+		experiencevar.style.display = "none";
+		portfoliovar.style.display = "none";
+		contactvar.style.display = "none";
+	} 
+}
+
+
+function myfunctionportfolio()
+{
+	if (portfoliovar.style.display === "none") {
+		portfoliovar.style.display = "block";
+		homevar.style.display = "none";
+		skillvar.style.display = "none";
+		experiencevar.style.display = "none";
+		educationvar.style.display = "none";
+		contactvar.style.display = "none";
+	} 
+}
+
+function myfunctioncontact()
+{
+	if (contactvar.style.display === "none") {
+		contactvar.style.display = "block";
+		homevar.style.display = "none";
+		skillvar.style.display = "none";
+		experiencevar.style.display = "none";
+		educationvar.style.display = "none";
+		portfoliovar.style.display = "none";
+	} 
+}
+
+
+
+
+
 setInterval(showTime, 1000);
 function showTime() {
     let time = new Date();
